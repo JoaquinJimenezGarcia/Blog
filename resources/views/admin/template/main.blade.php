89b581cc554dbf9009b8c1a4be71564bd3d66da1
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-07-03 02:51:25 +0200

    - Añadido Bootstrap - Empezando a crear el panel de administrador

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title') | Admin Panel</title>
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
+</head>
+<body>
+    @include('admin.template.partials.nav')
+    <section>
+        @yield('content')
+    </section>
+
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
